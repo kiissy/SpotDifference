cc5d4641e00f145bff7d8e19cbdb911080ac5f14
kiissy <kiissy@naver.com>
2020-09-27 16:56:50 +0900

    3. 함수로 틀린 곳 처리

--- SpotDifference/SpotDifference.py
+++ SpotDifference/SpotDifference.py
@@ -3,14 +3,28 @@ from bangtal import *
 setGameOption(GameOption.INVENTORY_BUTTON, False)
 setGameOption(GameOption.MESSAGE_BOX_BUTTON, False)
 
-Scene("틀린그림찾기", "images/problem.png")
+scene = Scene("틀린그림찾기", "images/problem.png")
 
 problem = Object("images/problem.png")
 problem.locate(scene, 0, 0)
 problem.show()
 
+check1_left = Object("images/check.png")
+check1_left.locate(scene, 568 - 25, 594 - 25)
+
+check1_right = Object("images/check.png")
+check1_right.locate(scene, 1186 - 25, 594 - 25)
+
+def checkIn(x, y, cx, cy, r):
+    return cx - r < x < cx + r and cy - r < y < cy + r
+
 def problem_onMouseAction(x, y, action):
-    endGame()
+    if checkIn(x, y, 568, 594, 54) or checkIn(x, y, 1186, 594, 54):
+        check1_left.show()
+        check1_right.show()
+    
+    else:
+        endGame()
 problem.onMouseAction = problem_onMouseAction
 
 showMessage("좌우에 틀린 곳을 찾아보세요.")
